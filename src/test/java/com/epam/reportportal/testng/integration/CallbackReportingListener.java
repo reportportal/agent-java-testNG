9e32c3eb30e749d9439df57fc3cc0d1310df88ec
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-04-14 10:44:50 +0300

    Build fix

--- src/test/java/com/epam/reportportal/testng/integration/CallbackReportingListener.java
+++ src/test/java/com/epam/reportportal/testng/integration/CallbackReportingListener.java
@@ -1,12 +1,8 @@
 package com.epam.reportportal.testng.integration;
 
-import com.epam.reportportal.listeners.ListenerParameters;
-import com.epam.reportportal.service.Launch;
 import com.epam.reportportal.service.ReportPortal;
 import com.epam.reportportal.testng.BaseTestNGListener;
-import com.epam.reportportal.testng.TestNGServiceExtension;
-import com.epam.reportportal.testng.integration.util.TestUtils;
-import com.epam.reportportal.utils.MemoizingSupplier;
+import com.epam.reportportal.testng.TestNGService;
 
 /**
  * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
@@ -16,9 +12,7 @@ public class CallbackReportingListener extends BaseTestNGListener {
 	public static final ThreadLocal<ReportPortal> REPORT_PORTAL_THREAD_LOCAL = new ThreadLocal<>();
 
 	public CallbackReportingListener() {
-		super(new TestNGServiceExtension(new MemoizingSupplier<>(() -> getLaunch(REPORT_PORTAL_THREAD_LOCAL.get().getParameters())),
-				REPORT_PORTAL_THREAD_LOCAL.get()
-		));
+		super(new TestNGService(REPORT_PORTAL_THREAD_LOCAL.get()));
 	}
 
 	public static void initReportPortal(ReportPortal reportPortal) {
@@ -27,10 +21,5 @@ public class CallbackReportingListener extends BaseTestNGListener {
 
 	public static ReportPortal getReportPortal() {
 		return REPORT_PORTAL_THREAD_LOCAL.get();
-	}
-
-	private static Launch getLaunch(ListenerParameters parameters) {
-		ReportPortal reportPortal = REPORT_PORTAL_THREAD_LOCAL.get();
-		return reportPortal.newLaunch(TestUtils.launchRQ(parameters));
 	}
 }
